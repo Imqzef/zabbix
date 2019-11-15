01af31617e18d3431668a5d861385fbe8510fd7b
Imqzef <39127838+Imqzef@users.noreply.github.com>
2019-11-15 13:35:50 +0800

    Update layout.htmlpage.menu.php
    4.5.1

--- frontends/php/app/views/layout.htmlpage.menu.php
+++ frontends/php/app/views/layout.htmlpage.menu.php
@@ -18,7 +18,7 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-
+/**
 $user_navigation = (new CList())
 	->addClass(ZBX_STYLE_TOP_NAV_ICONS)
 	->addItem(
@@ -155,3 +155,4 @@ if ($data['server_name'] !== '') {
 			->addItem($sub_menu_div)
 	)
 	->show();
+*/
